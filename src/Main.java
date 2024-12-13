508a28fbcbf1074a25ded61e0f0b2f84ed9b700d
rQuashie <kangoojuniors93@gmail.com>
2024-12-13 23:00:10 +0100

    feat() : Amélioration de la fonctionnalité match nul

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,24 +12,26 @@ public class Main {
         Scanner jDeux = new Scanner(System.in);
 
 
-        int tour = 1 ;
-        String emplacementJun , emplacementJdeux , vainqueur= "";
-        int compteCaseVide = 0 ;
+        int tour = 1;
+        String emplacementJun, emplacementJdeux = "";
+        boolean vainqueur = false, rempli = false;
+        int caseVide = 0;
 
 
         //Tableau
-        char [][] tab = new char[3][3];
+        char[][] tab = new char[3][3];
         String reponseJUn = "";
-        do{
+
+        do {
             //Nom des joueurs
             System.out.println("Joueur 1 veuillez saisir votre nom : ");
-            String  nomJUN = jUn.nextLine();
+            String nomJUN = jUn.nextLine();
             System.out.println("Joueur 2 veuillez saisir votre nom : ");
-            String  nomJDeux = jUn.nextLine();
+            String nomJDeux = jUn.nextLine();
 
             //COMMENCER PARTIE
-            while(tour <=9 ) {
-                tour ++ ;
+            while (tour <= 9) {
+                tour++;
                 //TOUR J1
                 System.out.println(nomJUN + " : veuillez choisir un emplacement (X)");
                 emplacementJun = jDeux.nextLine();
@@ -74,89 +78,114 @@ public class Main {
                 } else if (emplacementJdeux.equals("L3C3")) {
                     tab[2][2] = 'O';
                 }
-                System.out.println(Arrays.deepToString(tab));
+                for (int i = 0; i < tab.length; i++) {
+                    System.out.println(Arrays.toString(tab[i]) + '\n');
+                }
+
 
                 //Ligne 1
                 if (tab[0][0] == 'X' && tab[0][1] == 'X' && tab[0][2] == 'X') {
-                    vainqueur = nomJUN ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJUN);
                     break;
                 } else if (tab[0][0] == 'O' && tab[0][1] == 'O' && tab[0][2] == 'O') {
-                    vainqueur = nomJDeux;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJDeux);
                     break;
                 }
                 //Ligne 2
                 else if (tab[1][0] == 'X' && tab[1][1] == 'X' && tab[1][2] == 'X') {
-                    vainqueur = nomJUN ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJUN);
                     break;
                 } else if (tab[1][0] == 'O' && tab[1][1] == 'O' && tab[1][2] == 'O') {
-                    vainqueur = nomJDeux ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJDeux);
                     break;
                 }
                 //Ligne 3
                 else if (tab[2][0] == 'X' && tab[2][1] == 'X' && tab[2][2] == 'X') {
-                    vainqueur = nomJUN ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJUN);
                     break;
                 } else if (tab[2][0] == 'O' && tab[2][1] == 'O' && tab[2][2] == 'O') {
-                    vainqueur = nomJDeux ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJDeux);
                     break;
                 }
                 //Colonne 1
                 else if (tab[0][0] == 'X' && tab[1][0] == 'X' && tab[2][0] == 'X') {
-                    vainqueur = nomJUN ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJUN);
                     break;
                 } else if (tab[0][0] == 'O' && tab[1][0] == 'O' && tab[2][0] == 'O') {
-                    vainqueur = nomJDeux ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJDeux);
+                    break;
                 }
                 //Colonne 2
                 else if (tab[0][1] == 'X' && tab[1][1] == 'X' && tab[2][1] == 'X') {
-                    vainqueur = nomJUN ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJUN);
                     break;
                 } else if (tab[0][1] == 'O' && tab[1][1] == 'O' && tab[2][1] == 'O') {
-                    vainqueur = nomJDeux ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJDeux);
                     break;
                 }
                 //Colonne 3
                 else if (tab[0][2] == 'X' && tab[1][2] == 'X' && tab[2][2] == 'X') {
-                    vainqueur = nomJUN ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJUN);
                     break;
                 } else if (tab[0][2] == 'O' && tab[1][2] == 'O' && tab[2][2] == 'O') {
-                    vainqueur = nomJDeux ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJDeux);
                     break;
                 }
                 //DIAGONALE 1
                 else if (tab[0][0] == 'X' && tab[1][1] == 'X' && tab[2][2] == 'X') {
-                    vainqueur = nomJUN ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJUN);
                     break;
                 } else if (tab[0][0] == 'O' && tab[1][1] == 'O' && tab[2][2] == 'O') {
-                    vainqueur = nomJDeux ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJDeux);
                     break;
                 }
                 //DIAGONALE 2
                 else if (tab[0][2] == 'X' && tab[1][1] == 'X' && tab[2][0] == 'X') {
-                    vainqueur = nomJUN ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJUN);
                 } else if (tab[0][2] == 'O' && tab[1][1] == 'O' && tab[2][0] == 'O') {
-                    vainqueur = nomJDeux ;
+                    vainqueur = true;
+                    System.out.println("Vainqueur : " + nomJDeux);
                     break;
                 }
 
-                for(int i = 0 ; i < tab.length ; i++){
-                            for(int j = 0 ; j < tab.length ; j++){
-                                if (tab[i][j] != 'X' || tab[i][j] != 'O'){
-                                    compteCaseVide++ ;
-                                }
-                                if (compteCaseVide == 0){
-                                    vainqueur = "Match nul ";
-                                }
-                            }
-                        }
-
-
-
+                if (rempli(tab) == true && vainqueur == false) {
+                    System.out.println("Match nul");
+                    break;
+                }
             }
-            System.out.println("Le vainqueur est "+vainqueur);
-
-        System.out.println("Voulez-vous rejouer ? (o/n) :");
+            System.out.println("Voulez-vous rejouer ? (o/n) :");
             String reponseJun = jUn.nextLine().toUpperCase();
-    }while (reponseJUn.equals("O") || reponseJUn.equals('o'));
-        }
+
+        } while (reponseJUn.equals("O") || reponseJUn.equals('o'));
     }
+
+
+    public static boolean rempli(char[][] tab) {
+        int rempli= 0 ;
+        for (int i = 0; i < tab.length; i++) {
+            for(int j = 0; j < tab[i].length; j++) {
+             if (tab[i][j] == 'O' || tab[i][j] == 'X') {
+                rempli++;}
+            }
+        }
+        if(rempli == 9) {
+            return true;
+        }
+        return false ;
+    }
+}
